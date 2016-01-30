ff584602a6a440098a8d673bbee02148642f8847
satish28 <satish28888@gmail.com>
2016-01-30 18:39:32 +0530

    implemented sslscan, and info_disclosure in headers

--- checkheaders.py
+++ checkheaders.py
@@ -1,7 +1,16 @@
+"""
+Module to perform SSLscan for a URL, check HSTS
+, clickjacking headers & check server version
+& check HTTP methods using options.
+"""
 import requests
+import subprocess
 
-#checking if the app is vulnerable to clickjacking
+
 def check_clickjacking(url):
+    """
+    check if x-frame-options headers are implemented
+    """
     response = requests.get(url, verify=False)
     try:
         response_check_clickjacking = response.headers['x-frame-options']
@@ -15,6 +24,9 @@ def check_clickjacking(url):
 
 #checking is HSTS is implemented
 def check_hsts(url):
+    """
+    check if  HSTS headers are implemented
+    """
     response = requests.get(url, verify=False)
     try:
         response_check_hsts = response.headers['strict-transport-security']
@@ -25,8 +37,34 @@ def check_hsts(url):
 
 #perform sslscan for the url using sslscan and output it to a file.
 def sslscan(url, output_file):
-    pass
+    """
+    Perform SSLscan for the given url and output it to the given file
+    """
+    subprocess.call(["sslscan", "--no-failed", "--renegotiation",\
+                     "--xml=%s" %output_file, url])
 
 #check if server version is disclosed in headers
 def info_disclosure(url):
-    pass
+    """
+    Read the server headers and check if server version is disclosed
+    """
+    response = requests.get(url, verify=False)
+    try:
+        response_check_server_version = \
+                                        response.headers['server']
+        if response_check_server_version:
+            print "The server version disclosed is",\
+                response_check_server_version
+    except KeyError:
+        print "The repsonse headers are", response.headers
+
+#check http methods
+def check_httpoptions(url):
+    """
+    Use curl to check if TRACE and TRACK HTTP methods are implemented
+    """
+    check_options = requests.options(url, verify=False)
+    if check_options.status_code == 200:
+        print "Options are enabled %s", check_options.status_code
+    else:
+        print "Options not enabled. %s", check_options.status_code
